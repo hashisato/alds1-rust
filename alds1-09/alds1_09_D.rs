b2ffa1a0bd24739a53bac9c4eac909d5fa039929
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 23:38:44 +0900

    Update alds1_09_D.rs

--- alds1-09/alds1_09_D.rs
+++ alds1-09/alds1_09_D.rs
@@ -1,30 +1,40 @@
-use std::io::{self, BufRead};
+use std::io::*;
+use std::str::FromStr;
+
+// 標準入力から値を1つ読む
+fn rin<T: FromStr>() -> T {
+    let s = stdin();
+    let s = s.lock();
+    let s: String = s.bytes()
+        .map(|c| c.expect("failed reading char.") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    s.parse().ok().expect("failed parsing.")
+}
 
 fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-    let n: usize = lines.next().unwrap().unwrap().trim().parse().unwrap();
-    let mut a: Vec<i32> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let n: usize = rin();
+    let mut a: Vec<i32> = vec![0; n];
+    for i in 0..n {
+        a[i] = rin();
+    }
 
-    // 降順ソート
-    a.sort_by(|x, y| y.cmp(x));
+    // 昇順ソート
+    a.sort();
 
-    // ヒープのインデックス順に「幅優先」で値を割り当てる
-    let mut heap = vec![0; n + 1];
-    let mut idx = 0;
-    let mut q = vec![1];
-    while let Some(i) = q.pop() {
-        if i > n { continue; }
-        heap[i] = a[idx];
-        idx += 1;
-        q.insert(0, i * 2);     // left child
-        q.insert(0, i * 2 + 1); // right child
+    // ヒープ順に並べ替え
+    for i in 0..n-1 {
+        let mut j = i;
+        while j > 0 {
+            let k = (j-1)/2;
+            a.swap(j, k);
+            j = k;
+        }
+        a.swap(0, i+1);
     }
 
     // 出力
-    for i in 1..=n {
-        if i > 1 { print!(" "); }
-        print!("{}", heap[i]);
-    }
-    println!();
+    let result: Vec<String> = a.iter().map(|v| v.to_string()).collect();
+    println!("{}", result.join(" "));
 }
