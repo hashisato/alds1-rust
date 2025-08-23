3ac1b4ff988a25416d44a8e5d4aeda06233ec56c
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 23:40:02 +0900

    Update alds1_09_D.rs

--- alds1-09/alds1_09_D.rs
+++ alds1-09/alds1_09_D.rs
@@ -1,7 +1,6 @@
 use std::io::*;
 use std::str::FromStr;
 
-// 標準入力から値を1つ読む
 fn rin<T: FromStr>() -> T {
     let s = stdin();
     let s = s.lock();
@@ -15,26 +14,23 @@ fn rin<T: FromStr>() -> T {
 
 fn main() {
     let n: usize = rin();
-    let mut a: Vec<i32> = vec![0; n];
-    for i in 0..n {
-        a[i] = rin();
+    let mut a: Vec<i32> = Vec::with_capacity(n);
+    for _ in 0..n {
+        a.push(rin());
     }
 
-    // 昇順ソート
     a.sort();
 
-    // ヒープ順に並べ替え
-    for i in 0..n-1 {
+    for i in 0..n - 1 {
         let mut j = i;
         while j > 0 {
-            let k = (j-1)/2;
+            let k = (j - 1) / 2;
             a.swap(j, k);
             j = k;
         }
-        a.swap(0, i+1);
+        a.swap(0, i + 1);
     }
 
-    // 出力
     let result: Vec<String> = a.iter().map(|v| v.to_string()).collect();
     println!("{}", result.join(" "));
 }
